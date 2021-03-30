b9a46958be8420842dd1d90c5bce6ba47ede4394
rohman <ar886069@gmail.com>
2021-03-30 13:11:45 -0700

    halaman login register home OK

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Rohman <br> 361955401016
                 </div>
 
                 <div class="links">
